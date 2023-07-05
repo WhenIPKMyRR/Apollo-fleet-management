f4fe5b8ce1a200dfc4ee60a9a29b6e28dd935483
GABRIEL SOUSA DE OLIVEIRA <gabriel.oliveira20@alunos.sc.senac.br>
2023-07-04 22:00:51 -0300

    Implementa alterações nas Views

--- src/views/Brand/Brand.cs
+++ src/views/Brand/Brand.cs
@@ -123,33 +123,79 @@ namespace Views
 
             RefreshList();
 
+            TableLayoutPanel panel = new TableLayoutPanel();
+            panel.Dock = DockStyle.Bottom;
+            panel.AutoSize = true;
+            panel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panel.Padding = new Padding(10, 10, 10, 10);
+            panel.BackColor = ColorTranslator.FromHtml("#58ACFA");
+            panel.ColumnCount = 3;
+            panel.RowCount = 1;
+            panel.ColumnStyles.Clear();
+
+            for (int i = 0; i < panel.ColumnCount; i++)
+            {
+                panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            }
+
             Button btCrt = new Button();
             btCrt.Text = "Adicionar";
-            btCrt.Size = new Size(100, 30);
-            btCrt.Location = new Point(50, 330);
+            btCrt.Size = new Size(30, 30);
+            // btCrt.Location = new Point(50, 330);
+            btCrt.Font = new Font("Roboto", 8, FontStyle.Regular);
+            btCrt.FlatStyle = FlatStyle.Flat;
+            btCrt.FlatAppearance.BorderSize = 0;
+            btCrt.BackColor = ColorTranslator.FromHtml("#E0E6ED");
+            btCrt.ForeColor = ColorTranslator.FromHtml("#1c1c1e");
+            btCrt.Dock = DockStyle.Fill;
             btCrt.Click += new EventHandler(btCrt_Click);
-            this.Controls.Add(btCrt);
-
+            
             Button btUpdate = new Button();
             btUpdate.Text = "Editar";
-            btUpdate.Size = new Size(100, 30);
-            btUpdate.Location = new Point(170, 330);
+            btUpdate.Size = new Size(30, 30);
+            //btUpdate.Location = new Point(170, 330);
+            btUpdate.Font = new Font("Roboto", 8, FontStyle.Regular);
+            btUpdate.FlatStyle = FlatStyle.Flat;
+            btUpdate.FlatAppearance.BorderSize = 0;
+            btUpdate.BackColor = ColorTranslator.FromHtml("#E0E6ED");
+            btUpdate.ForeColor = ColorTranslator.FromHtml("#1c1c1e");
+            btUpdate.Dock = DockStyle.Fill;
             btUpdate.Click += new EventHandler(btUdpate_Click);
             this.Controls.Add(btUpdate);
 
             Button btDelete = new Button();
             btDelete.Text = "Deletar";
-            btDelete.Size = new Size(100, 30);
-            btDelete.Location = new Point(290, 330);
+            btDelete.Size = new Size(30, 30);
+            // btDelete.Location = new Point(290, 330);
+            btDelete.Font = new Font("Roboto", 8, FontStyle.Regular);
+            btDelete.FlatStyle = FlatStyle.Flat;
+            btDelete.FlatAppearance.BorderSize = 0;
+            btDelete.BackColor = ColorTranslator.FromHtml("#E0E6ED");
+            btDelete.ForeColor = ColorTranslator.FromHtml("#1c1c1e");
+            btDelete.Dock = DockStyle.Fill;
             btDelete.Click += new EventHandler(btDelete_Click);
             this.Controls.Add(btDelete);
 
             Button btClose = new Button();
-            btClose.Text = "Sair";
-            btClose.Size = new Size(100, 30);
-            btClose.Location = new Point(450, 330);
-            btClose.Click += new EventHandler(btClose_Click);
-            this.Controls.Add(btClose);
+            btClose.Text = "Voltar";
+            btClose.Size = new Size(30, 30);
+            // btClose.Location = new Point(410, 330);
+            btClose.BackColor = ColorTranslator.FromHtml("#E0E6ED");
+            btClose.ForeColor = ColorTranslator.FromHtml("#1c1c1e");
+            btClose.Font = new Font("Roboto", 8, FontStyle.Regular);
+            btClose.FlatStyle = FlatStyle.Flat;
+            btClose.FlatAppearance.BorderSize = 0;
+            btClose.Dock = DockStyle.Fill;
+            btClose.Click += (sender, s) =>
+            {
+                this.Close();
+            };
+            
+            panel.Controls.Add(btCrt, 0, 0);
+            panel.Controls.Add(btUpdate, 1, 0);
+            panel.Controls.Add(btDelete, 2, 0);
+            panel.Controls.Add(btClose, 3, 0); 
+            this.Controls.Add(panel);
         }
     }
 }
